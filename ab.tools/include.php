2549d7c741cf829499ac94b65295f6899da03539
dres <dres@1c.ru>
2017-03-28 16:51:42 +0300

    add mask plugin

--- ab.tools/include.php
+++ ab.tools/include.php
@@ -1,7 +1,7 @@
 <?php
 spl_autoload_register(function ($className) {
 	preg_match('/^(.*?)([\w]+)$/i', $className, $matches);
-	if (count($matches) < 3) {
+	if (count($matches) < 3){
 		return;
 	}
 
@@ -9,13 +9,13 @@ spl_autoload_register(function ($className) {
 		__DIR__,
 		"lib",
 		str_replace('\\', DIRECTORY_SEPARATOR, trim($matches[1], '\\')),
-		str_replace('_', DIRECTORY_SEPARATOR, $matches[2]) . '.php'
+		str_replace('_', DIRECTORY_SEPARATOR, $matches[2]).'.php',
 	));
-	$filePath = str_replace('AB'.DIRECTORY_SEPARATOR.'Tools'.DIRECTORY_SEPARATOR,'',$filePath);
-	$filePath = preg_replace('#AB\/Tools\/#','',$filePath);
-	$filePath = str_replace(DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, $filePath);
+	$filePath = str_replace('AB'.DIRECTORY_SEPARATOR.'Tools'.DIRECTORY_SEPARATOR, '', $filePath);
+	$filePath = preg_replace('#AB\/Tools\/#', '', $filePath);
+	$filePath = str_replace(DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, $filePath);
 
-	if (is_readable($filePath) && is_file($filePath)) {
+	if (is_readable($filePath) && is_file($filePath)){
 		/** @noinspection PhpIncludeInspection */
 		require_once $filePath;
 	}
@@ -33,7 +33,7 @@ spl_autoload_register(function ($className) {
 $path = '/local/modules/ab.tools/asset';
 $asset = [
 	'react' => [
-		'js' =>[
+		'js' => [
 //			$path.'/js/shim/es6-shim.min.js',
 //			$path.'/js/shim/es6-sham.min.js',
 			$path.'/js/react/react-with-addons.min.js',
@@ -41,20 +41,21 @@ $asset = [
 		],
 		'css' => [
 			$path.'/css/preloaders.css',
-			$path.'/css/animate.min.css'
-		]
+			$path.'/css/animate.min.css',
+		],
 	],
 	'isjs' => [
-		'js'=>[$path.'/js/is.min.js'],
+		'js' => [$path.'/js/is.min.js'],
 	],
 	'sweetalert' => [
 		'js' => [
 			$path.'/js/sweetalert.min.js',
 		],
 		'css' => [
-			$path.'/css/sweetalert.css'
-		]
+			$path.'/css/sweetalert.css',
+		],
 	],
+	'mask' => ['js' => $path.'/js/jquery.mask.min.js'],
 ];
 foreach ($asset as $name => $arItem) {
 	CJSCore::RegisterExt($name, $arItem);
